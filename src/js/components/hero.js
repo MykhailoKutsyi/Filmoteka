fec88d9c9ef222d86094fa648ac8d0e63d0ec349
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-05-03 15:06:50 +0300

    added works with api

---
+++ src/js/components/hero.js
@@ -0,0 +1,22 @@
+import markUpFilms from './markUpFilms';
+import { getTrendingMovies } from '../services/API';
+
+const refs = {
+  imagesList: document.querySelector('.films-list'),
+};
+pushFetch();
+
+function pushFetch() {
+  try {
+    const response = getTrendingMovies();
+    return response.then(data => {
+      markUp(data.data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function markUp(data) {
+  refs.imagesList.insertAdjacentHTML('beforeend', markUpFilms(data.results));
+}
