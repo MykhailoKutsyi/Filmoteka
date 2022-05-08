02a5555e0d7e388afa9714f06bf9028cfcb02784
AnatoliiSkuba <skuba.anatolii@gmail.com>
2022-05-08 15:20:49 +0300

    started to make spinner

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -1,7 +1,7 @@
-document.querySelector(".button-submit").disabled = true;
-document.querySelector(".submit-spinner").classList.remove('submit-spinner_hide');
+// document.querySelector(".button-submit").disabled = true;
+// document.querySelector(".submit-spinner").classList.remove('submit-spinner_hide');
   
 
-document.querySelector(".button-submit").disabled = false;
-document.querySelector(".submit-spinner").classList.add('submit-spinner_hide');
+// document.querySelector(".button-submit").disabled = false;
+// document.querySelector(".submit-spinner").classList.add('submit-spinner_hide');
   
