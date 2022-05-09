2f8150d135b2e5aac6a640ba9d3bbb16f1223e32
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-05-09 16:43:56 +0300

    Update API.js

--- src/js/services/API.js
+++ src/js/services/API.js
@@ -2,8 +2,8 @@ import { API_KEY, URL } from '../utils/constants';
 
 const axios = require('axios');
 
-export const getTrendingMovies = (page) => {
-  const url = `${URL}trending/movie/day?api_key=${API_KEY}&page=${page}`;
+export const getTrendingMovies = () => {
+  const url = `${URL}trending/movie/day?api_key=${API_KEY}`;
   const response = axios.get(url);
   return response;
 };
