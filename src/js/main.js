d491ae4fe70bd96445642a94bd8579a536e481c9
AnatoliiSkuba <skuba.anatolii@gmail.com>
2022-05-09 22:34:22 +0300

    fix dark theme

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,3 @@ import './components/card-modal';
 import './components/trailers';
 import './components/slider';
 import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
-<<<<<<< HEAD
-import './components/spinner';
-=======
->>>>>>> ffbdaa45fe97d310c95c81f3579e4e957fa84e36
