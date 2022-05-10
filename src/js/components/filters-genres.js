e16153af0e9f759e9bcc3bfbb55664d12e67203e
OlegSubotin <subotinoleg0@gmail.com>
2022-05-10 13:22:18 +0300

    filters

--- src/js/components/filters-genres.js
+++ src/js/components/filters-genres.js
@@ -1,80 +1,124 @@
+import { convertIdInGenre, movieGenresManipulationsMarkup } from "./genres";
+import { IMG_URL } from '../utils/constants';
+import { onCardsSelect } from './card-modal';
+
+
 const filtersContainer = document.querySelector('.filters');
 const filtersHideContainer = document.querySelector('.js-hide');
-let filtersListEl = document.querySelector('.js-filters-list');
+const filtersList = document.querySelector('.js-filters-list');
 let valueEl = document.getElementById('filters-values');
+const filtersBtn = document.querySelector('.filters__filter');
+let imagesList= document.querySelector('.films-list')
+
 const text = "You've chosen: ";
 const FILTERS_STORAGE_KEY = 'filters';
-const STORAGE_KEY = 'genres';
+const MOVIE_STORAGE_KEY = 'movies';
 let filters = [];
 
-markupGenresForFilters();
-checkForChosenGenres();
-
-// ================ filters container listener
-
+// ================ filters container listener=======================
 filtersContainer.addEventListener('mouseover', () => {
-    filtersHideContainer.classList.remove('filters_genres-hidden');
+    filtersHideContainer.classList.remove('filters__genres-hidden');
 });
 
 filtersContainer.addEventListener('mouseout', () => {
-    filtersHideContainer.classList.add('filters_genres-hidden');
+    filtersHideContainer.classList.add('filters__genres-hidden');
 });
 
-// ================ check in local storage if there are already chosen genres
-function checkForChosenGenres() {
-    if (localStorage.getItem(FILTERS_STORAGE_KEY)=== null) {
-        return;
-    } else {
-        let arr = localStorage.getItem(FILTERS_STORAGE_KEY);
-        JSON.parse(arr).forEach(el => {
-            filters.push(el);
-            let classElement = el.toString();
-            // console.log(classElement);
-           let element = document.querySelector(`.${classElement}`);
-            // console.log(element);
-            element.checked = true;
-            valueEl.innerHTML = text + filters.join(', ');
-            return filters;
-        });
-    };
-    
-};
-
+// ================ checkbox listener=================
 
 function onCheckboxClick() {
     if (this.checked == true) {
         filters.push(this.value);
-        valueEl.innerHTML = text + filters.join(', ');
+        valueEl.innerHTML = text + filters.map(filter => convertIdInGenre(Number(filter))).join(", ");
     } else {
         filters = filters.filter(element => element !== this.value);
-        valueEl.innerHTML = text + filters.join(', ');
+        valueEl.innerHTML = text + filters.map(filter=>convertIdInGenre(Number(filter))).join(", ");
     };
 
     if (filters.length === 0) {
         localStorage.removeItem(FILTERS_STORAGE_KEY);
+        document.location.reload();
     };
 
     if (filters.length !==0) {
         localStorage.setItem(FILTERS_STORAGE_KEY, JSON.stringify(filters));
-    };    
+    };  
+    return filters;
 };
-
 
 // =========================markup filters-genres ========================
 
 
-function markupGenresForFilters() {
-    let  arr = localStorage.getItem(STORAGE_KEY)
-    for (const el of JSON.parse(arr)) {
-        let filter = document.createElement('li');
-        filter.classList.add('filters_item');
-        filter.innerHTML = `
-        <input type="checkbox" value="${el.name}" class="filters_checkbox ${el.name} js-filters-check" id="${el.id}">
-            <label class="filters_label" for="${el.name}">${el.name}</label>
-        `;
-        let checkboxEl = filter.querySelector('.js-filters-check');
-        checkboxEl.addEventListener('click', onCheckboxClick);
-        filtersListEl.appendChild(filter);
-    };
+export function markupFiltersOfGenres(genre) {
+    const filtersMarkup = document.createElement("li");
+    filtersMarkup.classList.add("filters__item");
+    filtersMarkup.innerHTML = `
+        <input type="checkbox" value="${genre.id}" class="filters__checkbox js-filters-check" id="${genre.id}">
+        <label class="filters__label" for="${genre.id}">${genre.name}</label>
+    `;
+    filtersList.appendChild(filtersMarkup);
+
+    let checkboxEl = filtersMarkup.querySelector('.js-filters-check');
+
+    checkboxEl.addEventListener('click', onCheckboxClick);
 };
 
+// ================ check in local storage if there are already chosen genres
+
+export function checkForChosenGenres() {
+        if (localStorage.getItem(FILTERS_STORAGE_KEY) === null) {
+        // console.log('first')
+        return;
+    } else {
+        let arr = localStorage.getItem(FILTERS_STORAGE_KEY);
+        JSON.parse(arr).forEach(el => {
+            const element = document.querySelector(`input[id='${el}']`);
+            // console.log(element);
+            element.checked = true;
+            filters.push(element.value);
+            valueEl.innerHTML = text + filters.map(filter=>convertIdInGenre(Number(filter))).join(", ");
+            return filters;
+        });
+    };
+    return filters;
+};
+
+// ========================== button event=========
+
+filtersBtn.addEventListener('click', onFilterBtnClick);
+
+function onFilterBtnClick() {
+    if (filters.length === 0) {
+        filtersBtn.disabled = true;
+    } else {
+        let filterNum = filters.map(filter => Number(filter));
+        console.log(filterNum);
+        imagesList.innerHTML = '';
+        let allMovies = localStorage.getItem(MOVIE_STORAGE_KEY);
+        for (const movie of JSON.parse(allMovies)) {
+            let genreIds = movie.genre_ids;
+            let op = filterNum.every(element => genreIds.indexOf(element) > -1);
+            if (op === true) {
+                console.log(movie);
+                const movieEl = document.createElement('li');
+                movieEl.setAttribute('id', `${movie.id}`);
+                movieEl.classList.add('card-item');
+                let movieGenres = [];
+                for (let i = 0; i < movie.genre_ids.length; i += 1) {
+                let genre = convertIdInGenre(movie.genre_ids[i]);
+                movieGenres.push(genre);
+                };
+                movieEl.innerHTML = `
+                    <div class="card-item__image-box">
+                    <img src="${IMG_URL + movie.poster_path}" alt="Poster of ${movie.title}" class="card-item__image" />
+                    </div>
+                    <p class="card-item__text">${movie.title}<br />
+                    <span class="card-item__text--orange">${movieGenresManipulationsMarkup(movieGenres)} | ${movie.release_date?movie.release_date.slice(0, 4):''}</span>
+                    </p>  
+                `;
+                imagesList.appendChild(movieEl);
+                onCardsSelect();
+            };
+        };
+    };
+};
