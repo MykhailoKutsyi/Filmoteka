12b5a7d03ff316d75763f2ca6ce3182837ae7905
Nazariy Bakush <nazkamaz@gmail.com>
2022-05-15 13:49:06 +0300

    ntf about empty labrary

--- src/js/components/libraryRender.js
+++ src/js/components/libraryRender.js
@@ -1,10 +1,14 @@
 import { refsLibrary } from './header';
 import markUpFilms from './markUpFilms';
 import { onCardsSelect } from './card-modal';
+import { STORAGE_KEY_WATCHED, STORAGE_KEY_QUEUE } from '../utils/constants';
+import Swal from 'sweetalert2';
+// import { STORAGE_KEY_QUEUE } from '../utils/constants';
 
 export function renderWatchedLibrary(e) {
   e.preventDefault();
-  refsLibrary.watchedBtn.classList.add('library-btn-current');
+    refsLibrary.watchedBtn.classList.add('library-btn-current');
+  emptyWatchedLibNtf (e);
   refsLibrary.queueBtn.classList.remove('library-btn-current');
   const libData = JSON.parse(localStorage.getItem('WatchedLibrary'));
   markUpLib(libData);
@@ -15,6 +19,7 @@ export function renderQueueLibrary(e) {
   e.preventDefault();
   refsLibrary.watchedBtn.classList.remove('library-btn-current');
   refsLibrary.queueBtn.classList.add('library-btn-current');
+  emptyQueueLibNtf (e);
   const libData = JSON.parse(localStorage.getItem('QueueLibrary'));
   markUpLib(libData);
   refsLibrary.filtersWrapper.style.display = 'none';
@@ -25,3 +30,33 @@ function markUpLib(data) {
   refsLibrary.watchedList.insertAdjacentHTML('beforeend', markUpFilms(data));
   onCardsSelect();
 }
+function emptyWatchedLibNtf (e){
+  e.preventDefault();
+  if(refsLibrary.watchedBtn.classList.contains('library-btn-current') 
+  && localStorage.getItem(STORAGE_KEY_WATCHED) === '[]'
+  ){
+return Swal.fire({
+      position: 'top',
+      title: 'There are no movies in this library yet',
+      showConfirmButton: false,
+      timer: 2500,
+      background: 'darkgray',
+      color: 'black',
+    });
+  }
+  }
+  
+  function emptyQueueLibNtf (e){
+  e.preventDefault();
+  if(refsLibrary.queueBtn.classList.contains('library-btn-current') 
+  && localStorage.getItem(STORAGE_KEY_QUEUE) === '[]'
+  ){
+return Swal.fire({
+      position: 'top',
+      title: 'There are no movies in this library yet',
+      showConfirmButton: false,
+      timer: 2500,
+      background: 'darkgray',
+      color: 'black',
+    });
+  }};
