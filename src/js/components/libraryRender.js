942f932a483948469393ca456f9536af3b828bd8
Nazariy Bakush <nazkamaz@gmail.com>
2022-05-16 14:50:14 +0300

    fix bug empty library text

--- src/js/components/libraryRender.js
+++ src/js/components/libraryRender.js
@@ -1,7 +1,7 @@
 import { refsLibrary, imagesList, libraryNavEl } from './header';
 import { onCardsSelect } from './card-modal';
 import { STORAGE_KEY_WATCHED, STORAGE_KEY_QUEUE } from '../utils/constants';
-import Swal from 'sweetalert2';
+import markUpFilms from './markUpFilms';
 import {refs} from './hero';
 import { convertIdInGenre, movieGenresManipulationsMarkup } from './genres.js';
 import { IMG_URL} from '../utils/constants';
@@ -53,6 +53,7 @@ refs.emptyLibraryText.classList.add('visually-hidden');
   e.preventDefault();
   if(refsLibrary.queueBtn.classList.contains('library-btn-current') 
   && localStorage.getItem(STORAGE_KEY_QUEUE) === '[]'
+  && libraryNavEl.classList.contains('current')
   ){
 refs.emptyLibraryText.classList.remove('visually-hidden');
   }
@@ -61,41 +62,41 @@ refs.emptyLibraryText.classList.add('visually-hidden');
   }
   };
 
-function markUpFilms(data) {
-  if (!data) {
-    return;
-  }
+// function markUpFilms(data) {
+//   if (!data) {
+//     return;
+//   }
 
-  return data
-    .map(
-      ({
-        poster_path: posterPath,
-        genre_ids: genreIds,
-        id: movieId,
-        release_date: movieDate,
-        title: title,
-      }) => {
-        let movieGenres = [];
-        for (let i = 0; i < genreIds.length; i += 1) {
-          let genre = convertIdInGenre(genreIds[i]);
-          movieGenres.push(genre);
-        }
+//   return data
+//     .map(
+//       ({
+//         poster_path: posterPath,
+//         genre_ids: genreIds,
+//         id: movieId,
+//         release_date: movieDate,
+//         title: title,
+//       }) => {
+//         let movieGenres = [];
+//         for (let i = 0; i < genreIds.length; i += 1) {
+//           let genre = convertIdInGenre(genreIds[i]);
+//           movieGenres.push(genre);
+//         }
 
-        return `  
-       <li class="card-item" id="${movieId}">                
-        <div class="card-item__image-box">
-          <img src="${IMG_URL + posterPath}" alt="Poster of ${
-          title ? title : ''
-        }" onerror="this.src='https://michaelnakache.com/wp-content/uploads/2018/08/movie-poster-coming-soon-2.png';" class="card-item__image" />
-        </div>
-        <p class="card-item__text">${title ? title : 'Title unavailable'}<br />
-         <span class="card-item__text--orange">${movieGenresManipulationsMarkup(movieGenres)} | ${
-          movieDate ? movieDate.slice(0, 4) : 'Date unavailable'
-        }</span>
-        </p>   
-        </li>    
-       `;
-      },
-    )
-    .join('');
-}
+//         return `  
+//        <li class="card-item" id="${movieId}">                
+//         <div class="card-item__image-box">
+//           <img src="${IMG_URL + posterPath}" alt="Poster of ${
+//           title ? title : ''
+//         }" onerror="this.src='https://michaelnakache.com/wp-content/uploads/2018/08/movie-poster-coming-soon-2.png';" class="card-item__image" />
+//         </div>
+//         <p class="card-item__text">${title ? title : 'Title unavailable'}<br />
+//          <span class="card-item__text--orange">${movieGenresManipulationsMarkup(movieGenres)} | ${
+//           movieDate ? movieDate.slice(0, 4) : 'Date unavailable'
+//         }</span>
+//         </p>   
+//         </li>    
+//        `;
+//       },
+//     )
+//     .join('');
+// }
